5f844a678813ecccb8a93e0dcfa9496d16ce26cd
ameynuddin <md.ameynuddin@gmail.com>
2024-03-31 09:42:13 +0800

    added raw files

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function About() {
+    return (
+        <div class="about-section">
+            <h1 style={{ color: 'magenta' }}>About Us</h1>
+            <p>We are a partner of choice for some of the best brands in the Industrial and Motors sectors. With operations in 18 countries and territories with a strong footprint in Asia Pacific, we continue to grow, delivering sustainable value to our stakeholders through operational excellence, high performance standards and good corporate governance.</p>
+        </div>
+    )
+}
+
+export default About
